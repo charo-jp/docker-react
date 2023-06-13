5d9d8455f0be79604c424d20050716f9d9c71efa
Yuya Hochi <yuya1charo@gmail.com>
2023-06-13 16:16:50 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there This is charo hello fdsafdafda
+          Hi there This is charo hello fdsafdafda 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed on hte Feature branch
         </a>
       </header>
     </div>
